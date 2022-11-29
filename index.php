361f46dd6b5ed58cb49f7acd01ff0bd2f8a2eecc
kens1103 <118147062+kens1103@users.noreply.github.com>
2022-11-29 08:41:52 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Profil pribadi</title>
+        <style type="text/css"></style>
+
+    </head>
+    <body>
+        <h1 align="center">PROFIL</h1>
+        <hr width="800" style="2">
+        <table align="center" cellpadding="5" align="center" width="800">
+            <h1 align="center"><img src="kwai.jpg"></h1>
+            <tr>
+                <td> Nama Lengkap</td>
+                <td>: KENES CAHYANING WIBOWO</td>
+            </tr>
+            <tr>
+                <td> NIS</td>
+                <td>: 12200802</td>
+            </tr>
+            <tr>
+                <td> Rayon</td>
+                <td>: Al-Ikrom 1</td>
+            </tr>
+            <tr>
+                <td> Rombel/Jurusan</td>
+                <td>: X PPLG</td>
+            </tr>
+            <tr>
+                <td>Tempat, Tanggal Lahir</td>
+                <td>: Surabaya, 11 Maret 2007</td>
+            </tr>
+            <tr>
+                <td> Alamat</td>
+                <td>: SMK Wikrama 1 Garut, JL.Otto Iskandardinata, Kp.Tanjung, RT.003/RW.013, Pasawahan, Kec.Tarogong Kaler, Kab.Garut, Jawa Barat 44151</td>
+            </tr>
+            <tr>
+                <td> Jenis Kelamin</td>
+                <td>: Perempuan</td>
+            </tr>
+            <tr>
+                <td> Kewarganegaraan</td>
+                <td>: Indonesia</td>
+            </tr>
+            <tr>
+                <td> Agama</td>
+                <td>: Islam</td>
+            </tr>
+            <tr>
+                <td> Nomor Telpon</td>
+                <td>: 0856-xxxx-xxxx</td>
+            </tr>
+            <tr>
+                <td> Email</td>
+                <td>: kenesxxx@gmail.com</td>
+            </tr>
+            <tr>
+                <td> Berat & Tinggi Badan</td>
+                <td>: 49 & 159 cm</td>
+            </tr>
+            <tr>
+                <td> Hobi</td>
+                <td>: Membaca</td>
+            </tr>
+        </table>
+    </body>
+</html>
---
+++ indexcss.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>SMK WIKRAMA 1 GARUT</title>
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <nav>
+                <ul>
+                    <li class="logo"><a class="logo-text">SMK WIKRAMA 1 GARUT</a></li>
+                    <li><a class="active" href="#home">HISTORY</a></li>
+                    <li><a href="#">MATRICULATION</a></li>
+                    <li><a href="#">LIBRARY</a></li>
+                    <li><a href="#">CANTEEN</a></li>
+					<li><a href="#">HEALTHY FOOD</a></li>
+                </ul>
+            </nav>
+        </div>
+    </body>
+</html>
